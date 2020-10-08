5a29c69c0e91038e7f139961b68d22535a4e93d6
Bruno Marques <bmarques68@gmail.com>
2020-10-08 12:14:05 +0200

    up

--- tools/postinstall-fixup/macos-postinstall-fixup.sh
+++ tools/postinstall-fixup/macos-postinstall-fixup.sh
@@ -174,9 +174,11 @@ cp "$INSTALL_DIR/plugins/SofaQtQuickGUI/lib/libSofaQtQuickGUI.dylib" "$INSTALL_D
 
 # adding QML files for SofaQtQuick
 cp -Rf $QT_DIR/qml $INSTALL_DIR/
-cp -r $QT_DIR/lib/QtQmlWorkerScript.framework "$INSTALL_DIR/lib"
 cp -r $QT_DIR/lib/QtQuickControls2.framework "$INSTALL_DIR/lib"
 cp -r $QT_DIR/lib/QtQuickTemplates2.framework "$INSTALL_DIR/lib"
+cp -r $QT_DIR/lib/QtQml.framework "$INSTALL_DIR/lib"
+cp -r $QT_DIR/lib/QtQmlModels.framework "$INSTALL_DIR/lib"
+cp -r $QT_DIR/lib/QtQmlWorkerScript.framework "$INSTALL_DIR/lib"
 
 # adding SofaQtQuick.py to site-packages
 cp -r lib/python3/site-packages/{PythonConsole,SofaQtQuick,graph_serialization}.py "$INSTALL_DIR/lib/python3/site-packages"
